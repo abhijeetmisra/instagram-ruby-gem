3d5a11c4b1d9e8cab9aec2a9942d3608d7c85057
Abhijeet Misra <abhijeet.misra@gmail.com>
2012-08-14 20:17:54 +0530

    reverting response to response data in media_item coz its of no use

--- lib/instagram/client/media.rb
+++ lib/instagram/client/media.rb
@@ -18,7 +18,7 @@ module Instagram
       def media_item(*args)
         id = args.first || 'self'
         response = get("media/#{id}")
-        response
+        response["data"]
       end
 
       # Returns a list of the overall most popular media
